f5b440cda2408b2537c22c9a8bc8929491685319
RonxBulld <526677628@qq.com>
2020-02-07 23:36:41 +0800

    设计部分语法树解释器运行时接口

--- Compiler/interpreter/TreeInterpreter.h
+++ Compiler/interpreter/TreeInterpreter.h
@@ -6,10 +6,31 @@
 #define OPENELANGUAGE_TREEINTERPRETER_H
 
 #include "../opene_compiler/NodeDecl.h"
+#include "../opene_compiler/common_rt/ErrOr.h"
 
 namespace opene {
 
     class TIValue {
+    public: // 值分类断言
+        bool isNormalValue() const;
+        bool isStructureValue() const;
+
+    public: // 内置类型取值
+        ErrOr<char>                 getAsChar() const;
+        ErrOr<int>                  getAsInteger() const;
+        ErrOr<float>                getAsFloat() const;
+        ErrOr<bool>                 getAsBool() const;
+        ErrOr<std::string>          getAsString() const;
+        ErrOr<std::vector<char>>    getAsDataSet() const;
+        ErrOr<short>                getAsShort() const;
+        ErrOr<long>                 getAsLong() const;
+        ErrOr<time_t>               getAsDatetime() const;
+        ErrOr<FunctionDecl *>       getAsFuncPtr() const;
+        ErrOr<double>               getAsDouble() const;
+
+    public: // 结构体取值
+        bool hadMember(const std::string &member_name) const;
+        ErrOr<TIValue &> getMember(const std::string &member_name);
     };
 
     class TIVariable {
@@ -34,21 +55,40 @@ namespace opene {
 
     class TreeInterpreter {
     private:
+        RuntimeContext runtime_context_;
         TranslateUnit *translate_unit_ = nullptr;
+        Statement *execute_statement_ptr_ = nullptr;
 
-    private:
+    private:    // 变量加载
         bool LoadGlobalVariable();
         bool LoadFileVariable();
         bool LoadLocalVariable();
 
-    private:
+    private:    // 表达式执行
         TIValue CallFunction(FunctorDecl *functorDecl, std::vector<TIValue> arguments);
+        TIValue EvaluateExpression(Expression *expression);
+
+    private:    // 语句执行
+        bool ExecAssign(AssignStmt *assignStmt);
+        bool ExecLoop(LoopStatement *loopStatement);
+        bool ExecControl(ControlStmt *controlStmt);
+        bool ExecIf(IfStmt *ifStmt);
+        bool ExecStatementBlock(StatementBlock *statementBlock);
 
     public:
+        /*
+         * 加载翻译单元并做初始化
+         */
         bool LoadAST(TranslateUnit *translateUnit);
 
+        /*
+         * 卸载翻译单元并释放资源
+         */
         bool UnloadAST();
 
+        /*
+         * 开始执行
+         */
         bool Run();
     };
 
