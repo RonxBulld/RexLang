051ad37595a850bb473147322e845e8f1506968a
RonxBulld <526677628@qq.com>
2020-03-22 21:54:44 +0800

    开始为运行时库添加数组相关API

--- corelib/runtime_api.cpp
+++ corelib/runtime_api.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "runtime_api.h"
+
+#include <stdlib.h>
+
+char *create_array(int, ...) {
+    return nullptr;
+}
+
+void init_array_32bit(RTDynArrayTy, int, ...) {
+}
+
+int *get_array_ep(RTDynArrayTy, int, ...) {
+    return nullptr;
+}
--- corelib/runtime_api.h
+++ corelib/runtime_api.h
@@ -7,6 +7,12 @@
 
 extern "C" {
 
+typedef char *RTDynArrayTy;
+
+RTDynArrayTy create_array(int, ...);
+void init_array_32bit(RTDynArrayTy, int, ...);
+int *get_array_ep(RTDynArrayTy, int, ...);
+
 };
 
 #endif //OPENELANGUAGE_RUNTIME_API_H
