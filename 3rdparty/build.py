95738f5e11a1fe707bbc94f304357419fa891766
RonxBulld <526677628@qq.com>
2020-04-09 22:21:03 +0800

    去除Antlr动态库，保留静态库

--- 3rdparty/build.py
+++ 3rdparty/build.py
@@ -5,6 +5,14 @@ import os, shutil
 import platform
 
 ########################################
+# Function declare.
+########################################
+
+def remove_file_if_exist(path):
+    if os.path.isfile(path):
+        os.remove(path)
+
+########################################
 # Config files.
 ########################################
 
@@ -15,6 +23,7 @@ BuildArgs = {
         'ConfigTypes': 'Release',
         'ToolchainSpecify': '-A x64 -Thost=x64',
         'ParallelJobs': '',
+        'BuildDir': 'build-Windows',
     },
     'Linux': {
         'Sysroot': os.path.realpath(os.getcwd() + '/../sysroot'),
@@ -22,6 +31,7 @@ BuildArgs = {
         'ConfigTypes': 'Release',
         'ToolchainSpecify': '',
         'ParallelJobs': '8',
+        'BuildDir': 'build-Linux',
     }
 }
 
@@ -33,6 +43,7 @@ system = platform.system()
 if not system in BuildArgs:
     raise "Unknow system - " + system
 BuildArgs = BuildArgs[system]
+BuildArgs['BuildDir'] = BuildArgs['BuildDir'] + "-" + BuildArgs['BuildType']
 
 rootdir = os.getcwd()
 
@@ -41,13 +52,18 @@ rootdir = os.getcwd()
 ########################################
 
 os.chdir("antlr4-rt-cpp")
-if os.path.exists('build'):
-    shutil.rmtree("build", True)
-os.mkdir("build")
-os.chdir("build")
-os.system(f'cmake -DCMAKE_INSTALL_PREFIX={BuildArgs["Sysroot"]} -DCMAKE_BUILD_TYPE={BuildArgs["BuildType"]} -DCMAKE_CONFIGURATION_TYPES="{BuildArgs["ConfigTypes"]}" {BuildArgs["ToolchainSpecify"]} ..')
-os.system(f'cmake --build . --target antlr4_static --parallel {BuildArgs["ParallelJobs"]} --config {BuildArgs["BuildType"]}')
-os.system(f'cmake --build . --target antlr4_shared --parallel {BuildArgs["ParallelJobs"]} --config {BuildArgs["BuildType"]}')
-os.system('cmake --install .')
+# if os.path.exists(BuildArgs['BuildDir']):
+#     shutil.rmtree(BuildArgs['BuildDir'], True)
+if not os.path.exists(BuildArgs['BuildDir']):
+    os.mkdir(BuildArgs['BuildDir'])
+os.chdir(BuildArgs['BuildDir'])
+os.system(f'cmake -DCMAKE_INSTALL_PREFIX={BuildArgs["Sysroot"]} -DCMAKE_BUILD_TYPE={BuildArgs["BuildType"]} -DCMAKE_CONFIGURATION_TYPES="{BuildArgs["ConfigTypes"]}" -DWITH_STATIC_CRT=Off {BuildArgs["ToolchainSpecify"]} ..')
+os.system(f'cmake --build . --parallel {BuildArgs["ParallelJobs"]} --config {BuildArgs["BuildType"]}')
+os.system('cmake --install . --strip')
 os.chdir(rootdir)
-shutil.rmtree('antlr4-rt-cpp/build')
+# shutil.rmtree(f'antlr4-rt-cpp/{BuildArgs["BuildDir"]}')
+# Remove dynamic libraries.
+remove_file_if_exist('../sysroot/lib/libantlr4-runtime.so')
+remove_file_if_exist('../sysroot/lib/libantlr4-runtime.so.4.8')
+remove_file_if_exist('../sysroot/lib/antlr4-runtime.dll')
+shutil.copy2('../sysroot/lib/antlr4-runtime-static.lib', '../sysroot/lib/antlr4-runtime.lib')
