789b38604adc7dd9980d45294616eb22de143223
RonxBulld <526677628@qq.com>
2020-04-13 12:36:23 +0800

    添加了一些注释

--- Compiler/support/ProjectDB.h
+++ Compiler/support/ProjectDB.h
@@ -15,14 +15,42 @@ namespace rexlang {
     class ASTContext;
     class ArgumentParser;
 
+    /*!
+     * 项目数据库
+     * 包含众多与项目相关的信息
+     */
     class ProjectDB {
     private:
+        /*!
+         * 随机字符串
+         * 在初始化对象时被创建
+         * 当项目名称和生成目标名称未指定时使用此字符串替代
+         */
         std::string random_string_;
+        /*!
+         * 项目名称
+         * 如果未指定则使用随机字符串替代
+         */
         std::string project_name_;
+        /*!
+         * 项目中的文件列表
+         */
         std::vector<std::string> file_list_;
+        /*!
+         * 生成目标二进制文件名
+         */
         std::string target_bin_name_;
+        /*!
+         * 输入的翻译单元
+         */
         TranslateUnit *translate_unit_;
+        /*!
+         * 主入口函数名称
+         */
         std::string main_entry_name_;
+        /*!
+         * 主入口函数定义
+         */
         FunctorDecl *main_entry_;
 
     public:
