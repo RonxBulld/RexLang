e068cfe44793475d730806d77abaf7c0b969b946
RonxBulld <526677628@qq.com>
2020-04-15 18:01:26 +0800

    正确获取文档注释

--- libtools/CDecl/cdecl.cpp
+++ libtools/CDecl/cdecl.cpp
@@ -27,6 +27,9 @@ public:
 
     void run(const MatchFinder::MatchResult &Result) override {
         if (const FunctionDecl * functionDecl = Result.Nodes.getNodeAs<FunctionDecl>("funcDecl")) {
+
+            // 针对每个C函数声明
+
             if (functionDecl->isInvalidDecl() || functionDecl->isThisDeclarationADefinition()) {
 
                 // 不接受错误的声明或者带有定义的声明
@@ -35,27 +38,51 @@ public:
             }
 
             if (const RawComment *__funcdecl_raw_comment = functionDecl->getASTContext().getRawCommentForDeclNoCache(functionDecl)) {
+
+                // 针对注释信息
+
                 ASTContext &__func_ctx = functionDecl->getASTContext();
                 SourceManager &__func_source_manager = __func_ctx.getSourceManager();
                 std::string raw = __funcdecl_raw_comment->getRawText(__func_source_manager);
+
+                // 获取接口注释
+
                 std::string brief = __funcdecl_raw_comment->getBriefText(__func_ctx);
                 std::cout << functionDecl->getName().str() << "->" << brief << std::endl;
+
+                // 获取注释块列表
 
                 const comments::FullComment *__full_comment = functionDecl->getASTContext().getLocalCommentForDeclUncached(functionDecl);
                 auto __block_content_comments = __full_comment->getBlocks();
 
+                // 针对注释块
+
                 for (comments::BlockContentComment *__block_content_comment : __block_content_comments) {
                     if (comments::BlockCommandComment *__block_command_comment = llvm::dyn_cast<comments::BlockCommandComment>(__block_content_comment)) {
+
+                        // 针对指令性注释
+
                         unsigned int __args_cnt = __block_command_comment->getNumArgs();
                         for (unsigned int idx = 0; idx < __args_cnt; ++idx) {
-                            std::cout << __block_command_comment->getArgText(idx).str() << std::endl;
-                            std::cout << __block_command_comment->getParagraph()->child_count() << std::endl;
+
+                            // 取参数名
+
+                            std::string __parameter_name = __block_command_comment->getArgText(idx).str();
+
+                            // 获取参数对应注释信息
+
+                            comments::ParagraphComment *__paragraph_comment = __block_command_comment->getParagraph();
+                            std::stringstream __parameter_comment_sstr;
+                            for (auto iter = __paragraph_comment->child_begin(); iter != __paragraph_comment->child_end(); ++iter) {
+                                if (comments::TextComment *__text_comment = llvm::dyn_cast<comments::TextComment>(*iter)) {
+                                    __parameter_comment_sstr << __text_comment->getText().str();
+                                }
+                            }
+
+                            std::cout << __parameter_name << "->" << __parameter_comment_sstr.str() << std::endl;
                         }
                     }
                 }
-
-                int k = 0;
-
             }
         }
     }
