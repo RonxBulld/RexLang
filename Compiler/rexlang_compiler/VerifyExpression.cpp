68fa854421b89c2f99bda84ae62d08202e492263
RonxBulld <526677628@qq.com>
2020-11-22 23:50:54 +0800

    实现FunctionCall::matchFunctor

--- Compiler/rexlang_compiler/VerifyExpression.cpp
+++ Compiler/rexlang_compiler/VerifyExpression.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "NodeDecl.h"
+#include "rtti.h"
 
 #define VERIFY_SUB_EXPR(expr) if (!(expr)->VerifyExpression())
 
@@ -61,6 +62,120 @@ namespace rexlang {
         }
 
         return this->NameComponent::VerifyExpressionInternal();
+    }
+
+    bool FunctionCall::matchFunctor(FunctorDecl *functorDecl) const {
+        std::vector<Expression*> arguments = getArguments();
+        const std::vector<ParameterDecl*> &parameters = functorDecl->getParameters();
+
+        // 检查所有实参表达式
+
+        // 1. 获取形参有效个数
+
+        // 1.1. 获取个数
+
+        size_t argu_count = arguments.size();
+        size_t param_count = parameters.size();
+
+        // 1.2. 检查是否有不定参数
+
+        bool dynamic_params = functorDecl->hasVarArgs();
+        if (param_count > 0 && parameters.back()->isVariParam()) {
+            param_count -= 1;
+            dynamic_params = true;
+        }
+
+        // 2. 根据形参有效个数用空指针扩展实参
+
+        while (argu_count < param_count) {
+            arguments.push_back(nullptr);
+            argu_count++;
+        }
+
+        // 3. 如果形参长度不定则截取实参前N个进行计算
+
+        if (dynamic_params) {
+            argu_count = param_count;
+        }
+
+        // 4. 检查形参和实参个数是否匹配
+
+        if (argu_count != param_count) {
+            assert(false);
+            return false;
+        }
+
+        // 5. 针对每个实参/形参对
+
+        for (size_t idx = 0; idx < argu_count; idx++) {
+
+            Expression *          & argument  = arguments [idx];
+            ParameterDecl * const & parameter = parameters[idx];
+
+            if (argument == nullptr) {
+
+                // 5.1. 如果实参为空指针
+
+                // 5.1.1. 检查形参可空属性
+
+                if (!parameter->isNullable()) {
+                    assert(false);
+                    return false;
+                } else {
+                    continue;
+                }
+
+            }
+            /*else*/
+            if (parameter->type()->isArrayType() && argument != nullptr) {
+
+                // 5.2. 如果形参类型为数组，则实参必须为左值或左值引用（参考形参）数组变量，且元素类型严格一致
+                // 数组参数只能以引用方式传递
+
+                // 检查数组的元素类型是否匹配
+
+                TypeDecl *argu_ty  = argument->getExpressionType();
+                TypeDecl *param_ty = parameter->type();
+
+                if (!param_ty->compareTo(argu_ty)) {
+                    assert(false);
+                    return false;
+                }
+
+            }
+            /*else*/
+            if (parameter->shouldBeReference() && argument != nullptr) {
+
+                // 5.3. 如果形参参考属性为真，则实参必须为左值或左值引用（参考形参），并且变量类型严格一致
+
+                if (HierarchyIdentifier *hierarchy_identifier = argument->as<HierarchyIdentifier>()) {
+                    if (!parameter->type()->compareTo(hierarchy_identifier->getExpressionType())) {
+                        assert(false);
+                        return false;
+                    }
+                } else {
+                    assert(false);
+                    return false;
+                }
+
+            }
+            /*else*/
+            if (argument != nullptr) {
+
+                // 5.4. 如果形参不具备上述属性
+                // 以传值的方式检查，允许隐式类型提升
+
+                TypeDecl *param_ty = parameter->type();
+                if (Expression * implicit_convert = argument->castTo(param_ty)) {
+                    argument = implicit_convert;
+
+                } else {
+                    assert(false);
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     bool FunctionCall::VerifyExpressionInternal() {
