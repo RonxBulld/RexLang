7f159659e65b2ddd4a05f0943d163d16bf7593a1
RonxBulld <526677628@qq.com>
2020-11-29 17:38:47 +0800

    GenIRPass直接指向唯一的EmitLLIRPass

--- Compiler/ir/GenIRPass.cpp
+++ Compiler/ir/GenIRPass.cpp
@@ -4,19 +4,14 @@
 
 #include "../support/Pass.h"
 #include "../support/ProjectDB.h"
-#include "LLVMIR/EmitLLVMIR.h"
 
 namespace rexlang {
     class GenIRPass : public Pass {
     public:
-        GenIRPass() : Pass("GenIR") {}
+        GenIRPass() : Pass("EmitIR") {}
 
         int Call(ProjectDB &projectDB) override {
-            rexlang::EmitLLVMIR emitter(projectDB.getTranslateUnit(), projectDB);
-//        emitter.WriteOutIR();
-            rexlang::LLCodeGen ll_code_gen(emitter);
-//        if (int EC = ll_code_gen.WriteOutBitCode(projectDB.GetBitCodeFilename())) { return EC; }
-            if (int EC = ll_code_gen.WriteOutObject (projectDB.GetObjectFilename()))  { return EC; }
+            Pass::Call("EmitLLIR", projectDB);
             return 0;
         }
     };
