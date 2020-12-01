c1f7e9286d8b09272db6f3ca68a5576a3baa3d96
RonxBulld <526677628@qq.com>
2020-12-01 22:51:34 +0800

    定义一些AstLowing功能

--- Compiler/ir/LLVMIR/LowAST.cpp
+++ Compiler/ir/LLVMIR/LowAST.cpp
@@ -14,18 +14,50 @@ namespace rexlang {
         ProjectDB &project_db_ ;
 
     private:
-        int CreateGlobalObjectInitFunction() ;  // 创建全局对象初始化函数
+        // 创建启动函数
+        int CreateStartup() ;
+
+    private:
+        // 创建全局对象初始化函数
+        int CreateGlobalObjectInitFunction() ;
+
+        // 初始化全局数组
+        int InitializeGlobalArray() ;
+
+        // 初始化局部数组
+        int InitializeLocalArray() ;
+
+        // 初始化全局字符串/字节集
+        int InitializeGlobalString() ;
+
+        // 初始化局部字符串/字节集
+        int InitializeLocalString() ;
+
+    private:
+        // 重命名函数名
+        int MangleFunctionName() ;
+
+        // 重写字符串/字节集操作符
+        int OverrideStringOperator() ;
 
     public:
         explicit Lower(ProjectDB &projectDB) : project_db_(projectDB) {
+        }
+
+        int Run() {
+            int EC = 0;
+            if ((EC = CreateStartup())) { return EC; }
+            if ((EC = CreateGlobalObjectInitFunction())) { return EC; }
+            if ((EC = MangleFunctionName())) { return EC; }
+            if ((EC = OverrideStringOperator())) { return EC; }
+            return 0;
         }
 
     };
 
     int AstLowing(ProjectDB &projectDB) {
         Lower lower(projectDB);
-
-        return 0;
+        return lower.Run();
     }
 
 }
